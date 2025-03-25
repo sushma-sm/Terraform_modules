7e36271417f8d813c304a41b2d9fabf41c7e2325
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-25 16:06:04 +0530

    Update main.tf

--- modules/gcs_bucket/main.tf
+++ modules/gcs_bucket/main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "bucket" {
   name          = var.bucket_name
-  location      = var.bucket_location
+  location      = var.bucket_location  # Ensure this matches the variable name
   storage_class = var.storage_class
+  project       = var.project
 }
