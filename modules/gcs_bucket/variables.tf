470fbae16f6af3951fda6927fb1ded5d62e91aa4
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-25 11:47:34 +0530

    Update variables.tf

--- modules/gcs_bucket/variables.tf
+++ modules/gcs_bucket/variables.tf
@@ -1,3 +1,7 @@
+variable "project" {
+  description = "The GCP project ID where the bucket will be created"
+  type        = string
+}
 variable "bucket_name" {
   description = "Base name for the storage bucket"
   type        = string
