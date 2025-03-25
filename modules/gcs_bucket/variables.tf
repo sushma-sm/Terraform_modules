47fdc84294f123cea1a0dd12c296f870dd660190
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-25 16:00:33 +0530

    Update variables.tf

--- modules/gcs_bucket/variables.tf
+++ modules/gcs_bucket/variables.tf
@@ -1,3 +1,16 @@
-variable "bucket_name" {}
-variable "location" {}
-variable "project" {}
+variable "bucket_name" {
+  description = "The name of the GCS bucket"
+  type        = string
+}
+
+variable "bucket_location" {
+  description = "The location of the GCS bucket"
+  type        = string
+  default     = "US"
+}
+
+variable "storage_class" {
+  description = "The storage class of the bucket"
+  type        = string
+  default     = "STANDARD"
+}
