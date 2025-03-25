a9f9e1b5752ae7c3cdcd04ee917fd587c26db4da
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-25 11:22:44 +0530

    Update main.tf

--- modules/gcs_bucket/main.tf
+++ modules/gcs_bucket/main.tf
@@ -1,8 +1,4 @@
-resource "random_id" "bucket_suffix" {
-  byte_length = 6
-}
-
 resource "google_storage_bucket" "bucket" {
-  name     = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
+  name     = var.bucket_name
   location = var.location
 }
